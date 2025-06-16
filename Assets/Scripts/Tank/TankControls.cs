6a7e9d59e5cfff816d6a8b1b78080b75c6d8bfff
Aleksandr Ustinov <nnovruru@gmail.com>
2025-06-16 21:15:24 +0300

    Revert "indicators added"
    This reverts commit 841827b0869630e1bb2fb4f3a750c8a06f2da9db.

--- Assets/Scripts/Tank/TankControls.cs
+++ Assets/Scripts/Tank/TankControls.cs
@@ -144,7 +144,7 @@ namespace Tanks
                 {
                     ""name"": """",
                     ""id"": ""a3e20d11-77e3-4541-b572-d7e830eb926b"",
-                    ""path"": ""<Keyboard>/q"",
+                    ""path"": ""<Keyboard>/e"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
