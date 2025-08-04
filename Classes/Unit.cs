25abbf6a45aaa1880b5a495235371b9d16aea32a
Aleksandr Ustinov <nnovruru@gmail.com>
2025-08-04 20:24:35 +0300

    Unit.cs instead of Program.cs

---
+++ Classes/Unit.cs
@@ -0,0 +1,30 @@
+﻿namespace Classes
+{
+    public class Unit
+    {
+        private float _health;
+        public string Name { get; }
+        public float Health => _health;
+        public int Damage { get; }
+        public float Armor { get; }
+        
+        public Unit() : this(name:"Unknown Unit")
+        {
+        }
+        public Unit(string name, int damage = 5, float armor = 0.6f )
+        {
+            Name = name;
+            Damage = damage;
+            Armor = armor;
+        }
+        public float GetRealHealth()
+        {
+            return Health * (1f + Armor);
+        }
+        public bool SetDamage(float value)
+        {
+            _health = Health - value * Armor;
+            return (Health <= 0f);
+        }
+    }
+}
