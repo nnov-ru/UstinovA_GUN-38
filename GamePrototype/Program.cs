e5e7c54d8ed0e13b73e97cd126a4b4cf0c25097e
Aleksandr Ustinov <nnovruru@gmail.com>
2025-08-16 11:10:34 +0300

    initial commit

---
+++ GamePrototype/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GamePrototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
