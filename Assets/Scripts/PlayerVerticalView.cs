07b35131543d11f85cb149cdac0a49bcbc2bdd81
Aleksandr Ustinov <nnovruru@gmail.com>
2025-12-17 00:25:10 +0300

    new project (works well with VS)

---
+++ Assets/Scripts/PlayerVerticalView.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVerticalView : MonoBehaviour
+{
+    [SerializeField]
+    private 
+}
